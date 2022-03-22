635eb8fcfebbdcf98f8e28f4c0d285c8f794d803
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-21 22:00:16 -0300

    refactor: muda nome de variáveis de ambiente

--- 3.back-end/bloco24/24.3/nodejs-jwt-base-project/config/config.js
+++ 3.back-end/bloco24/24.3/nodejs-jwt-base-project/config/config.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
-const config = {
-  username: process.env.SQL_USERNAME,
-  password: process.env.SQL_PASSWORD,
-  host: process.env.SQL_HOST,
-  dialect: 'mysql',
-}
+const {
+  MYSQL_USER: username,
+  MYSQL_HOST: host,
+  MYSQL_PASSWORD: password,
+} = process.env;
+const dialect = 'mysql';
+
+const config = { username, password, host, dialect };
 
 module.exports = {
   development: {
