ebd5cbfc578b9b8712496ef7afeb72c1cadf25d3
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-04-11 00:45:54 -0300

    style: adiciona Document e Types

--- 3.back-end/bloco30/30.1/world-cups/src/schemas/tournaments.ts
+++ 3.back-end/bloco30/30.1/world-cups/src/schemas/tournaments.ts
@@ -1,25 +1,26 @@
-import { Schema } from 'mongoose';
+import { Schema, Document, Types } from 'mongoose';
 
-export interface Tournament {
+export interface Tournament extends Document {
   year: number,
   hostCountry: string,
   runnerUp: string,
   editionGoals: number,
   editionStrikers: string[],
-  bestPlayer: string,
-  bestGoalKeeper: string,
-  bestYoungPlayer: string,
+  bestPlayer?: string,
+  bestGoalKeeper?: string,
+  bestYoungPlayer?: string,
 }
 
 const cupSchema = new Schema<Tournament>({
+  id: { type: Types.ObjectId },
   year: { type: Number, required: true },
   hostCountry: { type: String, required: true },
   runnerUp: { type: String, required: true },
   editionGoals: { type: Number, required: true },
   editionStrikers: { type: [String], required: true },
-  bestPlayer: { type: String, required: true },
-  bestGoalKeeper: { type: String, required: true },
-  bestYoungPlayer: { type: String, required: true },
+  bestPlayer: { type: String, required: false },
+  bestGoalKeeper: { type: String, required: false },
+  bestYoungPlayer: { type: String, required: false },
 })
 
 export default cupSchema;
