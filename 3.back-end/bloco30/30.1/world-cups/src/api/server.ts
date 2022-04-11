acb33add67c8ece2e5d8a7d659f8a1e2dd3125b9
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-04-11 01:17:09 -0300

    style: muda string para number

--- 3.back-end/bloco30/30.1/world-cups/src/api/server.ts
+++ 3.back-end/bloco30/30.1/world-cups/src/api/server.ts
@@ -3,6 +3,6 @@ import 'dotenv/config';
 
 const app = new App().express;
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Ouvindo a porta \x1b[03;94m${port}\x1b[00m!`));
