db779a3f9707f77331214d5f07f12838e77b8024
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-05-27 17:31:48 -0300

    feat: conclui exercício 1 do dia 33.2

---
+++ 4.computer-science/bloco33/33.2/exercício1.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+nome = ''
+if len(argv) > 1:
+    for index in range(1, len(argv)):
+        if index > 1:
+            nome += ' '
+        nome += argv[index]
+else:
+    nome = input("Por favor, digite o seu nome:\n")
+
+def triangle_name(name):
+    result = name
+    for index in range(len(name), 0, -1):
+        print(name[0:index])
+
+triangle_name(nome)
