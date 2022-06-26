4301875fdfc95bb917e36333cac81a02a3effb35
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-06-26 19:03:59 -0300

    refactor: return error status to shell
    Return an error to the shell in case of timeout or any other error in
    the request.

--- 4.computer-science/bloco35/35.2/exercicios/exercicio1.py
+++ 4.computer-science/bloco35/35.2/exercicios/exercicio1.py
@@ -1,9 +1,15 @@
+from sys import exit
 import requests
 
 
 URL = "https://httpbin.org/encoding/utf8"
-def make_request(url):
-    response = requests.get(url)
+def make_request(url, timeout=5):
+    try:
+        response = requests.get(url)
+    except requests.HTTPError:
+        exit(1)
+    except requests.ReadTimeout:
+        exit(2)
     return response.text
 
 print(make_request(URL))
