439f78aa881daca971c12e9d3519ab0f2186c5b0
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-04 22:41:42 -0300

    feat: cria classe MainMemory

---
+++ 4.computer-science/bloco37/37.1/pc/main_memory.py
@@ -0,0 +1,12 @@
+class MainMemory:
+    def __init__(self):
+        self.clean()
+
+    def load(self, value):
+        # Sua implementação
+
+    def get(self, index):
+        # Sua implementação
+
+    def clean(self):
+        self.loaded_memory = []
