9fccaa074c7569ad7373ee2dc65587ad75a03dd7
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-05 22:36:07 -0300

    refactor: retorna saída apenas ao executar script

--- 4.computer-science/bloco37/37.1/resources.py
+++ 4.computer-science/bloco37/37.1/resources.py
@@ -14,16 +14,9 @@ desired_cpu_information = {
     "l2": "Cache L2",
     "l3": "Cache L3",
 }
-for desired_name, desired_description in desired_cpu_information.items():
-    for information in cpu_information:
-        if information.lower().startswith(desired_name):
-            information = information.split("  ")  # 2 blank spaces
-            information = information[-1].strip()
-            print(f"{desired_description}: {information}")
 
 
 # Memória
-
 def get_info(entry):
     info_list = entry.split(" ")
     spaces = 0
@@ -37,17 +30,28 @@ def get_info(entry):
 
 command = ["head", "-5", "/proc/meminfo"]
 memory_info = check_output(command).decode("UTF-8").split("\n")
-mem_total = get_info(memory_info[0])
-mem_free = get_info(memory_info[1])
-mem_available = get_info(memory_info[2])
-buffers = get_info(memory_info[3])
-cached = get_info(memory_info[4])
+mem_total = round(get_info(memory_info[0]) / 1024)
+mem_free = round(get_info(memory_info[1]) / 1024)
+mem_available = round(get_info(memory_info[2]) / 1024)
+buffers = round(get_info(memory_info[3]) / 1024)
+cached = round(get_info(memory_info[4]) / 1024)
 mem_used = mem_total - mem_free - buffers - cached
 memory_data = {
-    "Memória total": round(mem_total / 1024),
-    "Memória utilizada": round(mem_used / 1024),
-    "Memória livre": round(mem_free / 1024),
-    "Memória disponível": round(mem_available / 1024),
+    "Memória total": mem_total,
+    "Memória utilizada": mem_used,
+    "Memória livre": mem_free,
+    "Memória disponível": mem_available,
 }
-for label, value in memory_data.items():
-    print(f"{label}: {value} MB")
+
+
+# Output
+if __name__ == "__main__":
+    for desired_name, desired_description in desired_cpu_information.items():
+        for information in cpu_information:
+            if information.lower().startswith(desired_name):
+                information = information.split("  ")  # 2 blank spaces
+                information = information[-1].strip()
+                print(f"{desired_description}: {information}")
+
+    for label, value in memory_data.items():
+        print(f"{label}: {value} MB")
