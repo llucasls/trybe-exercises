4f71f4f25ec213f7d0bf89658b2cb98aa4abdf9c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-18 15:07:41 -0300

    feat: exercício 2 em andamento

---
+++ 4.computer-science/bloco38/38.2/exercicio2.py
@@ -0,0 +1,32 @@
+def biggest_substring(string):
+    streak = 0
+    record = 0
+    charset = set()
+    for char in string:
+        if char not in charset:
+            charset.add(char)
+            streak += 1
+            record = max(streak, len(charset))
+        else:
+            charset.clear()
+            charset.add(char)
+            streak = 1
+    return record
+
+
+def longer_no_repeating_substring_len(string):
+    biggest = 0
+    for index, _ in enumerate(string):
+        substr = set()
+        for letter in string[index:]:
+            if letter in substr:
+                break
+            substr.add(letter)
+        if len(substr) > biggest:
+            biggest = len(substr)
+    return biggest
+
+
+string = "serdevemuitolegalmasehprecisoestudarbastante"
+print(biggest_substring(string))
+print(longer_no_repeating_substring_len(string))
