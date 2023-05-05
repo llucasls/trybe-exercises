1cb683c2c0e3c29b960c3d2024def1c7911eb6ea
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-05-05 10:17:32 -0300

    feat: mostra quantidade disponível

---
+++ 4.computer-science/raspagem/exercicio5.py
@@ -0,0 +1,22 @@
+from requests import request
+from parsel import Selector
+
+
+URL = "http://books.toscrape.com/catalogue/the-grand-design_405/index.html"
+
+headers = {"Accept": "text/html", "User-Agent": "chrome"}
+with request("GET", URL, headers=headers) as response:
+    response.encoding = "uft-8"
+    selector = Selector(text=response.text)
+
+
+title = selector.css(".product_main h1::text").get()
+price = selector.css(".product_main p.price_color::text").get()
+description = (selector.css("#product_description ~ p::text").get()
+               .removesuffix("...more").strip())
+availability = (selector
+                .css("table.table.table-striped tr ~ *:nth-child(6) td::text")
+                .get().strip())
+
+print({"título": title, "preço": price, "descrição": description, "url": URL,
+       "disponíveis": availability})
